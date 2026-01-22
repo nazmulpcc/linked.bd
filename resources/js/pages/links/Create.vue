893fb4710a59a6c953986a803ca91452ab2e1264
Nazmul Alam <n@zmul.dev>
2026-01-22 20:25:47 +0600

    Fix link create form action binding

--- resources/js/pages/links/Create.vue
+++ resources/js/pages/links/Create.vue
@@ -48,7 +48,7 @@ const selectClass =
             <section class="rounded-2xl border border-border/70 bg-card p-6">
                 <Form
                     v-if="domains.length"
-                    v-bind="store.form()"
+                    v-bind="store()"
                     v-slot="{ errors, processing }"
                     class="grid gap-6"
                 >
