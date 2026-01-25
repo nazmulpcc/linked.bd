3cf9137d670cb357617f1127e4f7988e4a2f2423
Nazmul Alam <n@zmul.dev>
2026-01-25 22:56:17 +0600

    fix config file

--- config/scribe.php
+++ config/scribe.php
@@ -111,7 +111,7 @@ return [
         'default' => true,
 
         // Where is the auth value meant to be sent in a request?
-        'in' => AuthIn::BEARER->value,
+        'in' => class_exists(AuthIn::class) ? AuthIn::BEARER->value : 'bearer',
 
         // The name of the auth parameter (e.g. token, key, apiKey) or header (e.g. Authorization, Api-Key).
         'name' => 'Authorization',
